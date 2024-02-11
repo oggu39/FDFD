145ae1df594a29939ca11946e31ad7c7e3b91efe
ogbl00002 <oggu39@gmail.com>
2024-02-11 17:15:49 +0100

    added material initialization with pml and visualize materials

--- python/CPU/FDFD/FDFD2D.py
+++ python/CPU/FDFD/FDFD2D.py
@@ -1,5 +1,8 @@
+# Package needs numpy as scipy if run on cpu
 import numpy as np
 import scipy
+import matplotlib.pyplot as plt
+
 
 class FDFD_constants:
         c0 = float(299792458); # m/s
@@ -39,37 +42,40 @@ class FDFD_upml():
         
         
         
-    def upml_sgm(self): # Ficticious conductivity in the pml
-        w = np.linspace(0,1,self.pml_width); # Width along the pml
-        return self.sgm_max*(np.sin(0.5*np.pi*w)**2);
+    def upml_sgm(self,w): # Ficticious conductivity in the pml at the width w ~(0,1)
+        if(w >= 0 or w <= 1):
+            return self.sgm_max*(np.sin(0.5*np.pi*w)**2);
+        else:
+            raise Exception("Outside of the width of the pml sgm")
     
     
     
-    def upml_s0(self):
-        if(self.pml_width > 1):
-            w = np.linspace(0,1,self.pml_width); # Width along the pml
+    def upml_s0(self, w):
+        if(w >= 0 and w <= 1):
             return 1+self.s_max*(w**self.p); # w is length along the pml 0 - 1
         else:
-            raise Exception("Pml must have a width more than one node");
+            print(w)
+            raise Exception("Outside of the width of the pml s0");
             
             
             
-    def upml_s(self): 
-        if(self.pml_width > 1):
-            sgm = self.upml_sgm();
-            s0 = self.upml_s0();
-            s = s0*(1-1j*FDFD_constants.Z0*sgm); # S-par of pml along the width of pml
+    def upml_s(self,w): 
+        if(w >= 0 or w <= 1):
+            sgm = self.upml_sgm(w);
+            s0 = self.upml_s0(w);
+            s = s0*(1-1j*FDFD_constants.Z0*sgm); # S-par of pml along the width w of pml
+            return s;
         else:
-            raise Exception("Pml must have a width more than one node")
+            raise Exception("Pml must have a width more than one node s")
             
-        return s;
+        
         
         
 class FDFD_operators:
     
     def __init__(self, simDomain):
         # Initialise differential operator to empty sparse. Total number of nodes(unknowns) = Nx*Ny -> matrix size of Nx*Ny x Nx*Ny
-        self.Dxe = scipy.sparse.lil_matrix((simDomain.Nx*simDomain.Ny, simDomain.Nx*simDomain.Ny)); 
+        self.Dxe = scipy.sparse.lil_matrix((simDomain.Nx*simDomain.Ny, simDomain.Nx*simDomain.Ny)); # Might need to be complex type
         self.Dye = scipy.sparse.lil_matrix((simDomain.Nx*simDomain.Ny, simDomain.Nx*simDomain.Ny)); 
         self.Dxh = scipy.sparse.lil_matrix((simDomain.Nx*simDomain.Ny, simDomain.Nx*simDomain.Ny)); 
         self.Dyh = scipy.sparse.lil_matrix((simDomain.Nx*simDomain.Ny, simDomain.Nx*simDomain.Ny)); 
@@ -106,15 +112,105 @@ class FDFD_materials:
     def __init__(self, simDomain):
         self.Nx = simDomain.Nx;
         self.Ny = simDomain.Ny;
-        self.eps_xx = False;
-        self.eps_yy = False;
-        self.eps_zz = False;
-        self.mu_xx = False;
-        self.mu_yy = False;
-        self.mu_zz = False;
+        self.eps_xx = scipy.sparse.lil_matrix((self.Nx*self.Ny,self.Nx*self.Ny)).astype(complex);
+        self.eps_yy = scipy.sparse.lil_matrix((self.Nx*self.Ny,self.Nx*self.Ny)).astype(complex);
+        self.eps_zz = scipy.sparse.lil_matrix((self.Nx*self.Ny,self.Nx*self.Ny)).astype(complex);
+        self.mu_xx = scipy.sparse.lil_matrix((self.Nx*self.Ny,self.Nx*self.Ny)).astype(complex);
+        self.mu_yy = scipy.sparse.lil_matrix((self.Nx*self.Ny,self.Nx*self.Ny)).astype(complex);
+        self.mu_zz = scipy.sparse.lil_matrix((self.Nx*self.Ny,self.Nx*self.Ny)).astype(complex);
         
+    # Initialize entire grid to vacuumm
+    def init_materials(self, simDomain):
         
+        Nx = self.Nx;
+        Ny = self.Ny;
+        pml_width = simDomain.PML.pml_width;
+        eps_xx = np.ones((Nx*Ny,)).astype(complex);
+        eps_yy = np.ones((Nx*Ny,)).astype(complex);
+        eps_zz = np.ones((Nx*Ny,)).astype(complex);
 
+        
+        
+        mu_xx = np.ones((Nx*Ny,)).astype(complex);
+        mu_yy = np.ones((Nx*Ny,)).astype(complex);
+        mu_zz = np.ones((Nx*Ny,)).astype(complex);
+        
+        sx = np.ones((Nx*Ny,)).astype(complex);
+        sy = np.ones((Nx*Ny,)).astype(complex);
+        # sz = np.ones((Nx*Ny,)); Isn't needed in 2D
+        
+        # Incorporate pmls on all sides
+        # left side
+        for i in range(0,pml_width):
+            for j in range(0,Ny):
+                w = (pml_width-i)/pml_width;
+                q = i + j*Nx;
+                sx[q] = simDomain.PML.upml_s(w);
+
+        # right side
+        for i in range(Nx-pml_width,Nx,1):
+            for j in range(0,Ny):
+                w = (i-(Nx-pml_width))/pml_width;
+                q = i + j*Nx;
+                sx[q] = simDomain.PML.upml_s(w);
+                
+        
+        # bottom side
+        for i in range(0,Nx):
+            for j in range(0,pml_width):
+                w = (pml_width-j)/pml_width;
+                q = i+j*Nx;
+                sy[q] = simDomain.PML.upml_s(w);
+        
+        # top
+        for i in range(0,Nx):
+            for j in range(Ny-pml_width,Ny,1):
+                w = (j-(Ny-pml_width)+1)/pml_width;
+                q = i + j*Nx;
+                sy[q] = simDomain.PML.upml_s(w);
+                
+        
+        # Initialize materials in the entire grid to pml and vakum
+        self.eps_xx.setdiag(eps_xx*sy/sx,k=0);
+        self.eps_yy.setdiag(eps_yy*sx/sy,k=0);
+        self.eps_zz.setdiag(eps_zz*sx*sy,k=0);
+        
+        self.mu_xx.setdiag(mu_xx*sy/sx,k=0);
+        self.mu_yy.setdiag(mu_yy*sx/sy,k=0);
+        self.mu_zz.setdiag(mu_zz*sx*sy,k=0);
+   
+                
+    def get_materials(self):
+        Nx = self.Nx;
+        Ny = self.Ny;
+        eps_xx = np.ones((Nx,Ny)).astype(complex);
+        eps_yy = np.ones((Nx,Ny)).astype(complex);
+        eps_zz = np.ones((Nx,Ny)).astype(complex);
+        mu_xx = np.ones((Nx,Ny)).astype(complex);
+        mu_yy = np.ones((Nx,Ny)).astype(complex);
+        mu_zz = np.ones((Nx,Ny)).astype(complex);
+        
+        for i in range(0,Nx):
+            for j in range(0,Ny):
+                q = i+j*Nx;
+                eps_xx[i,j] = self.eps_xx[q,q];
+                eps_yy[i,j] = self.eps_yy[q,q];
+                eps_zz[i,j] = self.eps_zz[q,q];
+                
+                mu_xx[i,j] = self.mu_xx[q,q];
+                mu_yy[i,j] = self.mu_yy[q,q];
+                mu_zz[i,j] = self.mu_zz[q,q];
+                
+        return [eps_xx, eps_yy, eps_zz, mu_xx, mu_yy, mu_zz];
+    
+    def visualize_materials(self):
+        
+        materials = self.get_materials();
+        eps_z = np.abs(materials[2]);
+        plt.pcolor(eps_z,vmin=0,vmax=1000);
+        
+        
+       
         
         
         
--- python/CPU/main.py
+++ python/CPU/main.py
@@ -1,4 +1,29 @@
+import FDFD.FDFD2D as fdfd
 
+## Simulation domain parameters
+Lx = 50e-3;
+Ly = 50e-3;
+ds = 0.2e-3;
+
+## PML parameters
+x_bnd = True; 
+y_bnd = True;
+pml_width = 10;
+
+## Create simulation instance
+my_sim = fdfd.FDFD_simDomain('my_sim', Lx, Ly, ds)
+
+my_sim.snap_grid();# Snap simualtion to grid
+my_sim.PML = fdfd.FDFD_upml(pml_width, x_bnd, y_bnd) # Create 
+
+diff_operators = fdfd.FDFD_operators(my_sim);
+
+materials = fdfd.FDFD_materials(my_sim);
+materials.init_materials(my_sim);
+
+materials.visualize_materials();
+
+"""
 import FDFD.FDFD2D as fdfd;
 Lx = 600e-3;
 Ly = 6.2e-3;
@@ -14,5 +39,5 @@ diff_operators = fdfd.FDFD_operators(my_sim);
 
 freq = 10e9;
 diff_operators.FDFD_gen_operators(my_sim, freq);
-
+"""
 
